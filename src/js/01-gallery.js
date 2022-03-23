d8d3b45704f6b15884bac34bd1b89609aaa16502
Nataliia Lyfar <lifarnatala@gmail.com>
2022-03-23 21:21:55 +0200

    added library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,51 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+let instance = null;
+
+const galleryItem = galleryItems.map(el => {
+    return `<div class="gallery__item">
+    <a class="gallery__link"
+     href="${el.original}">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+    </a>
+  </div>`
+}).join('');
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItem);
+console.log(galleryContainer);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+function onImageClick(e) {
+    e.preventDefault(); 
+    // if (e.target.nodeName !== 'IMG') {
+    //     return;
+    // }
+    // console.log(e.target);
+   
+    instance = basicLightbox.create(`<img
+    class="gallery__image"
+    data-source="${e.target.dataset.source}"
+  />`,
+   {
+    onShow: () => {
+       window.addEventListener('keydown', onEscPress);
+    },
+    onClose: () => {
+        window.removeEventListener('keydown', onEscPress);
+     },
+});
+instance.show();
+}
+
+function onEscPress(e){
+if(e.code === 'Escape'){instance.close();}
+};
